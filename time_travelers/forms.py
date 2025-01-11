1ade1143cbde0f38359becadb7e517e2a056122e
h-alizadeh2 <hajar466@gmail.com>
2025-01-11 14:51:41 +0330

    multiple changes

--- time_travelers/forms.py
+++ time_travelers/forms.py
@@ -1,5 +1,6 @@
 # import form class from django
 from django import forms
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Time_Traveler
 
 # create a ModelForm
@@ -7,3 +8,17 @@ class Create_Time_traveler(forms.ModelForm):
     class Meta:
         model = Time_Traveler
         fields = "__all__"
+
+def TravelerUpdate(request,pk):
+    traveler = get_object_or_404(Time_Traveler , id=pk)
+
+    if request.method == 'POST':
+
+        form = Create_Time_traveler(request.POST,instance=traveler)
+        if form.is_valid ():
+            form.save()
+            return redirect ('form')
+    else:
+        form = Create_Time_traveler(instance=traveler) 
+    
+    return render(request, 'update.html', {'form':form})       
